d1c5722885beb9207d2ab741b9d7b8d41628f68f
Riley Houghton <HougRile352@GoTVDSB.ca>
2021-10-21 10:34:10 -0400

    FINALLY DONE

--- 1.7 part 2/1.7 part 2.cpp
+++ 1.7 part 2/1.7 part 2.cpp
@@ -1,46 +1,52 @@
 // 1.7 part 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// I hate this assignment so much - Riley
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-void formattingFunction(string a, double b, string c, string d, int e, int f, int g)
+void formattingFunction(string a, string b, double c, string d, string e, int f, int g, string h)
 {
-    cout << "Author name:" + a << endl;
+    cout << b << ", " << a << ". (" << c << ") " << d << "\n"; //Formats the data into something readable
+    cout << "           " << e << ", " << f << "(" << g << "), " << h;
 }
 
 int main()
 {
-    string AuthorName; int Year;
+    string AuthorName; int Year; string LastName; //Initialize variables
     string ArticleTitle;
-    string JournalTitle; float VolumeNumber; int IssueNumber; int PageCount;
+    string JournalTitle; float VolumeNumber; int IssueNumber; string PageCount; 
 
-    cout << "Enter the author's name!\n";
-    cin >> AuthorName;
+    cout << "Enter the author's first name!\n"; // Get the data
+    getline(cin, AuthorName); //cin doesn't support spaces so I had to do getline
 
-    cout << "Enter year:";
-    cin >> Year;
+    cout << "Enter the author's last name!\n"; //I would've gotten both in one input but that was too much of a headache sorry
+    getline(cin, LastName);
 
-    cout << "Enter article title:";
-    cin >> ArticleTitle;
+    cout << "Enter article title:\n";
+    getline(cin, ArticleTitle);
 
-    cout << "Enter journal title:";
-    cin >> JournalTitle;
+    cout << "Enter journal title:\n";
+    getline(cin, JournalTitle);
 
-    cout << "Enter volume number:";
+    cout << "Enter pages (#-# format):\n";
+    getline(cin, PageCount);
+
+    cout << "Enter year:\n";
+    cin >> Year; //Left all the raw numbers to the end to fix a pesky bug
+
+    cout << "Enter volume number:\n";
     cin >> VolumeNumber;
 
-    cout << "Enter issue number:";
+    cout << "Enter issue number:\n";
     cin >> IssueNumber;
 
-    cout << "Enter number of pages:";
-    cin >> PageCount;
+    string AuthorInitial = AuthorName.substr(0, 1); //Gets the initial from the name
 
-    string AuthorInitial = AuthorName.substr(0);
+    cout << "\n"; //newline :)
 
-    formattingFunction(AuthorInitial, Year, ArticleTitle, JournalTitle, VolumeNumber, IssueNumber, PageCount);
+    formattingFunction(AuthorInitial, LastName, Year, ArticleTitle, JournalTitle, VolumeNumber, IssueNumber, PageCount); //Run the format function from earlier
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
